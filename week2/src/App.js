38822e526697435c0671c23140c397ee7a7908bf
Andrew <ageremia@email.neit.edu>
2022-05-04 18:53:06 -0400

    form validation

--- week2/src/App.js
+++ week2/src/App.js
@@ -1,31 +1,22 @@
 import './App.css';
 import { useState } from 'react';
+import Form from './components/form';
 
 function App() {
 
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
 
-  const submit = (event) => {
-    event.preventDefault();
-    setFirstName("");
-    setLastName("");
-    setEmail("");
+  const [formHidden, setFormHidden] = useState(true);
+
+  const toggleForm = () => {
+    setFormHidden(!formHidden);
   }
+
 
   return (
     <div>
-      <form onSubmit={submit}>
-        <label>First Name</label>
-        <input name="first" value={firstName} onChange={(event) => setFirstName(event.target.value)}></input>
-        <label>Last Name</label>
-        <input name="last" value={lastName} onChange={(event) => setLastName(event.target.value)}></input>
-        <label>Email</label>
-        <input name="email" value={email} onChange={(event) => setEmail(event.target.value)}></input>
-        <input type="submit"></input>
-      </form>
-      
+      <button onClick={toggleForm}>Assignment</button>
+      {!formHidden && (<Form setFormHidden={setFormHidden}></Form>)}
+
     </div>
   );
 }
